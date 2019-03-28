37ffe1bffe6bde9fbdfedeb1fcffb10c2eee69e6
BudiNverse <budisyahiddin@gmail.com>
2019-03-29 04:28:20 +0800

    Updated Tests

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,7 @@ pub mod train;
 #[cfg(test)]
 mod tests {
     use std::env;
+    use std::fmt::Debug;
 
     use crate::{bus, crowd, taxi, traffic, train};
     use crate::client_config::CLIENT_CONFIG;
@@ -24,223 +25,102 @@ mod tests {
     use crate::traffic::carpark_avail::Carpark;
     use crate::traffic::traffic_speed_bands::TrafficSpeedBand;
 
-    #[test]
-    fn get_arrivals() {
+    fn run_test_and_print<F, T>(f: F)
+        where F: Fn() -> reqwest::Result<T>,
+              T: Debug
+    {
         let api_key = env::var("API_KEY").unwrap();
         CLIENT_CONFIG.lock().unwrap().with_api_key(api_key.as_str());
-
-        let resp = bus::get_arrival(83139, "15");
-        match resp {
-            Ok(bus_arrival_resp) => println!("{:?}", bus_arrival_resp),
+        let res = f();
+        match res {
+            Ok(r) => println!("{:?}", r),
             Err(e) => println!("{:?}", e)
         };
+    }
+
+    #[test]
+    fn get_arrivals() {
+        run_test_and_print(|| bus::get_arrival(83139, "15"));
     }
 
     #[test]
     fn get_bus_services() {
-        let api_key = env::var("API_KEY").unwrap();
-        CLIENT_CONFIG.lock().unwrap().with_api_key(api_key.as_str());
-
-        let resp = bus::get_bus_services();
-        match resp {
-            Ok(r) => println!("{:?}", r),
-            Err(e) => println!("{:?}", e)
-        };
+        run_test_and_print(|| bus::get_bus_services());
     }
 
     #[test]
     fn get_bus_routes() {
-        let api_key = env::var("API_KEY").unwrap();
-        CLIENT_CONFIG.lock().unwrap().with_api_key(api_key.as_str());
-
-        let resp = bus::get_bus_routes();
-        match resp {
-            Ok(r) => println!("{:?}", r),
-            Err(e) => println!("{:?}", e)
-        };
+        run_test_and_print(|| bus::get_bus_routes())
     }
 
     #[test]
     fn get_bus_stops() {
-        let api_key = env::var("API_KEY").unwrap();
-        CLIENT_CONFIG.lock().unwrap().with_api_key(api_key.as_str());
-
-        let resp = bus::get_bus_stops();
-        match resp {
-            Ok(r) => println!("{:?}", r),
-            Err(e) => println!("{:?}", e)
-        };
+        run_test_and_print(|| bus::get_bus_stops());
     }
 
     #[test]
     fn get_passenger_vol() {
-        let api_key = env::var("API_KEY").unwrap();
-        CLIENT_CONFIG.lock().unwrap().with_api_key(api_key.as_str());
-
-        let resp = crowd::get_passenger_vol_by(VolType::OdTrain);
-        match resp {
-            Ok(r) => println!("{:?}", r),
-            Err(e) => println!("{:?}", e)
-        };
+        run_test_and_print(|| crowd::get_passenger_vol_by(VolType::OdTrain))
     }
 
     #[test]
     fn get_taxi_avail() {
-        let api_key = env::var("API_KEY").unwrap();
-        CLIENT_CONFIG.lock().unwrap().with_api_key(api_key.as_str());
-
-        let resp = taxi::get_taxi_avail();
-        match resp {
-            Ok(r) => println!("{:?}", r),
-            Err(e) => println!("{:?}", e)
-        };
+        run_test_and_print(|| taxi::get_taxi_avail())
     }
 
     #[test]
     fn get_erp_rates() {
-        let api_key = env::var("API_KEY").unwrap();
-        CLIENT_CONFIG.lock().unwrap().with_api_key(api_key.as_str());
-
-        let resp = traffic::get_erp_rates();
-        match resp {
-            Ok(r) => println!("{:?}", r),
-            Err(e) => println!("{:?}", e)
-        };
+        run_test_and_print(|| traffic::get_erp_rates());
     }
 
     #[test]
     fn get_cp_avail() {
-        let api_key = env::var("API_KEY").unwrap();
-        CLIENT_CONFIG.lock().unwrap().with_api_key(api_key.as_str());
-
-        let resp = traffic::get_carpark_avail();
-        match resp {
-            Ok(r) => {
-                println!("{:?}", r);
-                let only_none: Vec<Carpark> = r.into_iter().filter(|p| {
-                    p.coords.is_none()
-                }).collect();
-                println!("{:?}", only_none);
-            }
-            Err(e) => println!("{:?}", e)
-        };
+        run_test_and_print(|| traffic::get_carpark_avail());
     }
 
     #[test]
     fn get_est_travel_time() {
-        let api_key = env::var("API_KEY").unwrap();
-        CLIENT_CONFIG.lock().unwrap().with_api_key(api_key.as_str());
-
-        let resp = traffic::get_est_travel_time();
-        match resp {
-            Ok(r) => println!("{:?}", r),
-            Err(e) => println!("{:?}", e)
-        };
+        run_test_and_print(traffic::get_est_travel_time());
     }
 
     #[test]
     fn get_faulty_traffic_lights() {
-        let api_key = env::var("API_KEY").unwrap();
-        CLIENT_CONFIG.lock().unwrap().with_api_key(api_key.as_str());
-
-        let resp = traffic::get_faulty_traffic_lights();
-        match resp {
-            Ok(r) => println!("{:?}", r),
-            Err(e) => println!("{:?}", e)
-        };
+        run_test_and_print(|| traffic::get_faulty_traffic_lights());
     }
 
     #[test]
     fn get_road_details() {
-        let api_key = env::var("API_KEY").unwrap();
-        CLIENT_CONFIG.lock().unwrap().with_api_key(api_key.as_str());
-
-        let resp = traffic::get_road_details(traffic::road::RoadDetailsType::RoadWorks);
-        match resp {
-            Ok(r) => println!("{:?}", r),
-            Err(e) => println!("{:?}", e)
-        };
+        run_test_and_print(|| traffic::get_road_details(traffic::road::RoadDetailsType::RoadWorks));
     }
 
     #[test]
     fn get_traffic_images() {
-        let api_key = env::var("API_KEY").unwrap();
-        CLIENT_CONFIG.lock().unwrap().with_api_key(api_key.as_str());
-
-        let resp = traffic::get_traffic_images();
-        match resp {
-            Ok(r) => println!("{:?}", r),
-            Err(e) => println!("{:?}", e)
-        };
+        run_test_and_print(|| traffic::get_traffic_images());
     }
 
     #[test]
     fn get_traffic_incidents() {
-        let api_key = env::var("API_KEY").unwrap();
-        CLIENT_CONFIG.lock().unwrap().with_api_key(api_key.as_str());
-
-        let resp = traffic::get_traffic_incidents();
-        match resp {
-            Ok(r) => println!("{:?}", r),
-            Err(e) => println!("{:?}", e)
-        };
+        run_test_and_print(|| traffic::get_traffic_incidents());
     }
 
     #[test]
     fn get_traffic_speed_band() {
-        let api_key = env::var("API_KEY").unwrap();
-        CLIENT_CONFIG.lock().unwrap().with_api_key(api_key.as_str());
-
-        let resp = traffic::get_traffic_speed_band();
-        match resp {
-            Ok(r) => {
-                println!("{:?}", r);
-                let only_none: Vec<TrafficSpeedBand> = r.into_iter().filter(|p| {
-                    p.coord_start_end.is_none()
-                }).collect();
-
-                println!("{:?}", only_none);
-            }
-            Err(e) => println!("{:?}", e)
-        };
+        run_test_and_print(|| traffic::get_traffic_speed_band());
     }
 
     #[test]
     fn get_vms() {
-        let api_key = env::var("API_KEY").unwrap();
-        CLIENT_CONFIG.lock().unwrap().with_api_key(api_key.as_str());
-
-        let resp = traffic::get_vms_emas();
-        match resp {
-            Ok(r) => println!("{:?}", r),
-            Err(e) => println!("{:?}", e)
-        }
+        run_test_and_print(|| traffic::get_vms_emas());
     }
 
 
     #[test]
     fn get_bike_parking() {
-        let api_key = env::var("API_KEY").unwrap();
-        CLIENT_CONFIG.lock().unwrap().with_api_key(api_key.as_str());
-
-        let resp =
-            traffic::get_bike_parking(1.364897, 103.766094, 0.5);
-
-        match resp {
-            Ok(r) => println!("{:?}", r),
-            Err(e) => println!("{:?}", e)
-        }
+        run_test_and_print(|| traffic::get_bike_parking(1.364897, 103.766094, 0.5));
     }
 
     #[test]
     fn get_train_service_alerts() {
-        let api_key = env::var("API_KEY").unwrap();
-        CLIENT_CONFIG.lock().unwrap().with_api_key(api_key.as_str());
-
-        let resp = train::get_train_service_alert();
-        match resp {
-            Ok(r) => println!("{:?}", r),
-            Err(e) => println!("{:?}", e)
-        }
+        run_test_and_print(|| train::get_train_service_alert())
     }
 }
