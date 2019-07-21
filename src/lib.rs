0609e527f3b7e0b11827b9ad16a7a6b2c6de5f40
Budi Syahiddin <budisyahiddin@protonmail.ch>
2019-07-21 13:19:52 +0800

    Updated lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 //!
 //! ## Design Decisions
 //! - Made sure that Rust structs are as close to the original response as possible to make sure that people can reference the original docs if there are any issues
-//! - Simple and no additional baggage. Only the client is included. E.g If anyone wants to add concurrency, they have to do it their own
+//! - Simple and no additional baggage. Only the client is included. E.g If anyone wants to add concurrency, they have to do it on their own
 //!
 //! ## Usage
 //! Put this in you `Cargo.toml`
@@ -61,87 +61,104 @@ mod tests {
 
     #[test]
     fn get_arrivals() {
+        println!("get_arrivals");
         run_test_and_print(|c| bus::get_arrival(c, 83139, "15"))
     }
 
     #[test]
     fn get_bus_services() {
+        println!("get_bus_services");
         run_test_and_print(|c| bus::get_bus_services(c));
     }
 
     #[test]
     fn get_bus_routes() {
+        println!("get_bus_routes");
         run_test_and_print(|c| bus::get_bus_routes(c));
     }
 
     #[test]
     fn get_bus_stops() {
+        println!("get_bus_stops");
         run_test_and_print(|c| bus::get_bus_stops(c));
     }
 
     #[test]
     fn get_passenger_vol() {
+        println!("get_passenger_vol");
         run_test_and_print(|c| crowd::get_passenger_vol_by(c, VolType::OdTrain));
     }
 
     #[test]
     fn get_taxi_avail() {
+        println!("get_taxi_avail");
         run_test_and_print(|c| taxi::get_taxi_avail(c));
     }
 
     #[test]
     fn get_erp_rates() {
+        println!("get_erp_rates");
         run_test_and_print(|c| traffic::get_erp_rates(c));
     }
 
     #[test]
     fn get_cp_avail() {
+        println!("get_cp_avail");
         run_test_and_print(|c| traffic::get_carpark_avail(c));
     }
 
     #[test]
     fn get_est_travel_time() {
+        println!("get_est_travel_time");
         run_test_and_print(|c| traffic::get_est_travel_time(c));
     }
 
     #[test]
     fn get_faulty_traffic_lights() {
+        println!("get_faulty_traffic_lights");
         run_test_and_print(|c| traffic::get_faulty_traffic_lights(c));
     }
 
     #[test]
     fn get_road_details() {
+        println!("get_road_details");
         use traffic::road::RoadDetailsType::RoadWorks;
         run_test_and_print(|c| traffic::get_road_details(c, RoadWorks));
     }
 
     #[test]
     fn get_traffic_images() {
+        println!("get_traffic_images");
         run_test_and_print(|c| traffic::get_traffic_images(c));
     }
 
     #[test]
     fn get_traffic_incidents() {
+        println!("get_traffic_incidents");
         run_test_and_print(|c| traffic::get_traffic_incidents(c));
     }
 
     #[test]
     fn get_traffic_speed_band() {
+        println!("get_traffic_speed_band");
         run_test_and_print(|c| traffic::get_traffic_speed_band(c));
     }
 
     #[test]
     fn get_vms() {
+        println!("get_vms");
         run_test_and_print(|c| traffic::get_vms_emas(c));
     }
 
     #[test]
     fn get_bike_parking() {
+        println!("get_bike_parking");
         run_test_and_print(|c| traffic::get_bike_parking(c, 1.364897, 103.766094, 0.5));
     }
 
     #[test]
     fn get_train_service_alerts() {
+        println!("get_train_service_alerts");
         run_test_and_print(|c| train::get_train_service_alert(c));
     }
 }
